b7641c32f99e36358b67c8da125c4ca9fb208152
Ovidiu Negoescu <ovidiunegoescu@gmail.com>
2026-06-16 15:41:48 +0300

    Increase font sizes globally

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,9 +11,16 @@ const config: Config = {
         sans: ["var(--font-dm-sans)", "sans-serif"],
         serif: ["var(--font-fraunces)", "serif"],
       },
+      fontSize: {
+        'xs': ['0.8rem', { lineHeight: '1.2rem' }],
+        'sm': ['0.95rem', { lineHeight: '1.4rem' }],
+        'base': ['1.05rem', { lineHeight: '1.6rem' }],
+        'lg': ['1.15rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.3rem', { lineHeight: '1.9rem' }],
+        '2xl': ['1.55rem', { lineHeight: '2rem' }],
+      },
     },
   },
   plugins: [],
 };
-
 export default config;
