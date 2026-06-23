39493399af79be8607d2de92a7d08f7519fc8ef8
Ovidiu Negoescu <ovidiunegoescu@gmail.com>
2026-06-23 15:49:45 +0300

    optimizare imagini

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'edvdtvywwjqzgzwrldet.supabase.co',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
